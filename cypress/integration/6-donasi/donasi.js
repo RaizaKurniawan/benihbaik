c1583b0c1719a88d22d372e205f034c9394ff392
RaizaKurniawan <raizakurniawan@gmail.com>
2022-04-17 15:16:28 +0700

    all payment

--- cypress/integration/6-donasi/donasi.js
+++ cypress/integration/6-donasi/donasi.js
@@ -31,10 +31,84 @@ class Donasi {
         return cy 
             .get('#jumlah-donasi').click()
     }
-    klikbcapayment(klikbcapayment) {
-        return cy 
-            .get('[onclick="buttonPayment(8)"]').click()
-    }
+    /* Bagian Payment */
+        // Pembayaran Instan
+        klikbcapayment(klikbcapayment) {
+            return cy 
+                .get('[onclick="buttonPayment(8)"]').click()
+        }
+        ovopayment(){
+            return cy 
+                .get('[onclick="buttonPayment(11)"]')
+        }
+        gopaypayment() {
+            return cy
+                .get('[onclick="buttonPayment(10)"]')
+        }
+        shopeepay(){
+            return cy
+                .get('[onclick="buttonPayment(26)"]')
+        }
+        danapayment(){
+            return cy
+                .get('[onclick="buttonPayment(3)"]')
+        }
+        bcaklikpay(){
+            return cy
+                .get('[onclick="buttonPayment(6)"]')
+        }
+        linkaja(){
+            return cy
+                .get('[onclick="buttonPayment(103)"]')
+        }
+        linkajasyariah(){
+            return cy
+                .get('[onclick="buttonPayment(104)"]')
+        }
+        brimopayment(){
+            return cy
+                    .get('[onclick="buttonPayment(12)"]')
+        }
+        // Transfer Virtual Account
+        bcava(){
+            return cy.get('[onclick="buttonPayment(5)"]')
+        }
+        bniva(){
+            return cy.get('[onclick="buttonPayment(108)"]')
+        }
+        virtuallainnya(){
+            return cy.get('[onclick="buttonPayment(25)"]')
+        }
+        danamonva(){
+            return cy.get('[onclick="buttonPayment(110)"]')
+        }
+        briva(){
+            return cy.get('[onclick="buttonPayment(13)"]')
+        }
+        /// TRansfer Manual
+        bankBCA(){
+            return cy.get('[onclick="buttonPayment(1)"]')
+        }
+        bankMandiri(){
+            return cy.get('[onclick="buttonPayment(2)"]')
+        }
+        bankBRI(){
+            return cy.get('[onclick="buttonPayment(14)"]')
+        }
+        bankBNI(){
+            return cy.get('[onclick="buttonPayment(17)"]')
+        }
+        bankOCBC(){
+            return cy.get('[onclick="buttonPayment(20)"]')
+        }
+        bankDBS(){
+            return cy.get('[onclick="buttonPayment(23)"]')
+        }
+        bankBJB(){
+            return cy.get('[onclick="buttonPayment(28)"]')
+        }
+
+    /* */
     inputnama(inputnamalengkap){
         return cy 
             .get('[style="padding: 0px 20px 5px 20px;"] > :nth-child(1) > .form-control')
